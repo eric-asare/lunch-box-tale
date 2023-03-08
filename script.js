23da310821ff3992bfba018e69219c696f3ceab8
Eric Asare <70132474+eric-asare@users.noreply.github.com>
2023-03-08 22:08:04 +0400

    changes

--- script.js
+++ script.js
@@ -13,10 +13,76 @@ function isInViewport(element) {
     );
 }
 
-document.querySelector('.alarm').addEventListener('click', () => {
+// scene1
+// deSVG('.scene1', true);
+// document.getElementById('scene1').getElementsByClassName('path')[0].style.backgroundColor = 'red';
+
+sounds_alarm = document.getElementById('sounds_x5F_1');
+sounds_alarm.style.display = 'none';
+
+document.getElementById('button_x5F_1').addEventListener('click', () => {
     console.log("mouse over")
-    document.getElementById("beep").play();
+    beep = document.getElementById("beep");
+    beep.play();
+    if (beep.duration > 0 && !beep.paused) {
+        sounds_alarm.style.display = 'block';
+        sounds_alarm.classList.add('bounce2');
+    }
+
 });
+
+
+document.getElementById("beep").addEventListener("ended", () => {
+    sounds_alarm.style.display = 'none';
+    sounds_alarm.classList.remove('bounce2');
+});
+
+
+// scene2
+// hide awake scene
+awake = document.getElementById('awake_x5F_2');
+awake.style.display = 'none';
+// make snoring happen
+document.getElementById('bubbles_x5F_2').addEventListener('click', () => {
+    snore = document.getElementById("snore");
+    snore.play();
+});
+
+
+document.getElementById('button_x5F_2').addEventListener('click', () => {
+    // snoring stops, awake scene appears, bubles disappear
+    snore = document.getElementById("snore");
+    snore.pause();
+    awake.style.display = 'block';
+
+    document.getElementById('bubbles_x5F_2').style.display = 'none';
+    document.getElementById('button_x5F_2').style.display = 'none';
+});
+
+
+
+
+// sounds_snore = document.getElementById('sounds_x5F_2');
+// sounds_alarm.style.display = 'none';
+
+// document.getElementById('button_x5F_2').addEventListener('click', () => {
+//     snore = document.getElementById("snore");
+//     snore.play();
+//     if (beep.duration > 0 && !beep.paused) {
+//         sounds_alarm.style.display = 'block';
+//         sounds_alarm.classList.add('bounce2');
+//     }
+
+// });
+
+
+// document.getElementById("beep").addEventListener("ended", () => {
+//     sounds_alarm.style.display = 'none';
+//     sounds_alarm.classList.remove('bounce2');
+// });
+
+
+
 
 document.querySelector('.alarm').addEventListener('mouseout', () => {
     document.getElementById("beep").pause();
